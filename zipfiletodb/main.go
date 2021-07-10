726ba4ec75b536c52818e7bc7a9749d76e8a7639
Anissa Siti Rahma <anissa.rahma@dataon.com>
2021-07-10 16:27:27 +0700

    baca file per baris

--- zipfiletodb/main.go
+++ zipfiletodb/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"archive/zip"
+	"bufio"
+	"log"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -12,4 +17,58 @@ type Row struct {
 	ShortType    string    `db:"short_type"`
 	Size         int       `db:"size"`
 	Price        float64   `db:"price"`
+}
+
+func main() {
+	// open zip file
+	zipFileName := "files/FNSQsh202105_1.zip"
+	zr, err := zip.OpenReader(zipFileName)
+	if err != nil {
+		log.Fatalln("failed to read", zipFileName, "with error:", err)
+	}
+	defer zr.Close()
+
+	// read each file inside zip file
+	for _, f := range zr.File {
+		file, err := f.Open()
+		if err != nil {
+			log.Fatalln("failed to open", f.Name, "with error:", err)
+		}
+		defer file.Close()
+
+		// read file by line
+		scanner := bufio.NewScanner(file)
+		scanner.Split(bufio.ScanLines)
+		for scanner.Scan() {
+
+			// convert line into Row struct
+			t := strings.Split(scanner.Text(), "|")
+			date, err := time.Parse("20060102", t[2])
+			if err != nil {
+				continue
+			}
+			time, err := time.Parse("15:04:05", t[3])
+			if err != nil {
+				continue
+			}
+			size, err := strconv.Atoi(t[5])
+			if err != nil {
+				continue
+			}
+			price, err := strconv.ParseFloat(t[6], 64)
+			if err != nil {
+				continue
+			}
+			row := Row{
+				MarketCenter: t[0],
+				Symbol:       t[1],
+				Date:         date,
+				Time:         time,
+				ShortType:    t[4],
+				Size:         size,
+				Price:        price,
+			}
+			log.Println("ROW:", row)
+		}
+	}
 }
