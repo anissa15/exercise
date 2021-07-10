83ab6143c45cbb8bb3bd3519aa6ebb4d48d60405
Anissa Siti Rahma <anissa.rahma@dataon.com>
2021-07-10 16:32:19 +0700

    populate baris file per batch

--- zipfiletodb/main.go
+++ zipfiletodb/main.go
@@ -39,36 +39,71 @@ func main() {
 		// read file by line
 		scanner := bufio.NewScanner(file)
 		scanner.Split(bufio.ScanLines)
-		for scanner.Scan() {
 
-			// convert line into Row struct
-			t := strings.Split(scanner.Text(), "|")
-			date, err := time.Parse("20060102", t[2])
-			if err != nil {
-				continue
-			}
-			time, err := time.Parse("15:04:05", t[3])
-			if err != nil {
-				continue
-			}
-			size, err := strconv.Atoi(t[5])
-			if err != nil {
-				continue
-			}
-			price, err := strconv.ParseFloat(t[6], 64)
-			if err != nil {
-				continue
-			}
-			row := Row{
-				MarketCenter: t[0],
-				Symbol:       t[1],
-				Date:         date,
-				Time:         time,
-				ShortType:    t[4],
-				Size:         size,
-				Price:        price,
-			}
-			log.Println("ROW:", row)
+		// populate data per batch
+		batch := 100
+		p := initPopulate(scanner, batch)
+		for p.Scan() {
+			log.Println("jumlah data per scan :", len(p.rows))
 		}
 	}
+}
+
+type populate struct {
+	scanner *bufio.Scanner
+	batch   int
+	done    bool
+	rows    []Row
+}
+
+func initPopulate(scanner *bufio.Scanner, batch int) *populate {
+	return &populate{scanner: scanner, batch: batch}
+}
+
+func (p *populate) Scan() bool {
+	if p.done {
+		return false
+	}
+
+	var rows []Row
+	for p.scanner.Scan() {
+		// convert line into Row struct
+		t := strings.Split(p.scanner.Text(), "|")
+		date, err := time.Parse("20060102", t[2])
+		if err != nil {
+			continue
+		}
+		time, err := time.Parse("15:04:05", t[3])
+		if err != nil {
+			continue
+		}
+		size, err := strconv.Atoi(t[5])
+		if err != nil {
+			continue
+		}
+		price, err := strconv.ParseFloat(t[6], 64)
+		if err != nil {
+			continue
+		}
+		row := Row{
+			MarketCenter: t[0],
+			Symbol:       t[1],
+			Date:         date,
+			Time:         time,
+			ShortType:    t[4],
+			Size:         size,
+			Price:        price,
+		}
+		rows = append(rows, row)
+
+		// check rows size
+		if len(rows) >= p.batch {
+			p.rows = rows
+			return true
+		}
+	}
+
+	p.rows = rows
+	p.done = true
+	return true
 }
